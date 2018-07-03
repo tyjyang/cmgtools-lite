081bad01cd1d6ad249375b56cc091a1d31a6b198
Marco Cipriani <cippy91@gmail.com>
2018-07-03 18:10:05 +0200

    adding option to exclude names matching a regular expression; other fixes in the style of summary plot (colors, line width etc)

--- WMass/python/plotter/w-helicity-13TeV/plotParametersFromToys.py
+++ WMass/python/plotter/w-helicity-13TeV/plotParametersFromToys.py
@@ -2,6 +2,7 @@
 
 ## USAGE
 ## python plotParametersFromToys.py toys_wminus.root --pdir plots --suffix minus
+## use option ' -a diffXsec ' for differential cross section analysis
 
 import ROOT, os, re, datetime
 from sys import argv, stdout, stderr, exit
@@ -10,21 +11,52 @@ ROOT.gROOT.SetBatch(True)
 import utilities
 utilities = utilities.util()
 
+# import few functions from plotUtils/utility.h
+from sys import path as syspath
+syspath.append(os.getcwd() + "/plotUtils/")
+from utility import addStringToEnd,createPlotDirAndCopyPhp
+
+from make_diff_xsec_cards import get_ieta_ipt_from_process_name
+
+
 lat = ROOT.TLatex(); lat.SetNDC()
 
-def plotPars(inputFile, pois=None, selectString='', maxPullsPerPlot=30, plotdir='./', suffix=''):
+def plotPars(inputFile, pois=None, selectString='', maxPullsPerPlot=30, plotdir='./', suffix='', analysis='helicity', excludeName=None):
  
-    valuesAndErrors = utilities.getHistosFromToys(inputFile)
-   
-    channel = 'mu' if any(re.match(param,'.*_mu_Ybin_.*') for param in valuesAndErrors.keys()) else 'el'
+    # warning: utilities.getHistosFromToys is working only for parameters centered around 0 (histogram is defined in -3,3)
+    # this script will not work if using parameters with pmaskedexp
+    if pois:
+        if any (["pmaskedexp" in x for x in pois.split(",")]):
+            print "Warning: current setup cannot work with parameters matching pmaskedexp: they are expected to be centered around 0. Exit"
+            print "You might want to change behaviour of function utilities.getHistosFromToys to use those parameters as well" 
+            exit(0)
+    
+    all_valuesAndErrors = utilities.getHistosFromToys(inputFile)
+    
+    matchString = ".*_mu_Ybin_.*" if analysis == "helicity" else ".*_mu_group_.*"
+    channel = 'mu' if any(re.match(param,matchString) for param in all_valuesAndErrors.keys()) else 'el'
     print "From the list of parameters it seems that you are plotting results for channel ",channel
+    
+    print "pois = ", pois
+
+    valuesAndErrors = {}
+
+    excludedNames = excludeName.split(',') if excludeName else []
 
     if pois:
         poi_patts = pois.split(",")
-        for ppatt in poi_patts:
-            valuesAndErrors = dict((k,v) for (k,v) in valuesAndErrors.iteritems() if re.match(ppatt,k))
+        for ppatt in poi_patts:            
+            for (k,v) in all_valuesAndErrors.iteritems():
+                if any([re.match(x,k) for x in excludedNames]): continue
+                if re.match(ppatt,k) and k not in valuesAndErrors: valuesAndErrors[k] = v
+            #print valuesAndErrors.keys()
 
     params = valuesAndErrors.keys()
+    print "----------------------------------"
+    print "List of parameters:"
+    print params
+    print "----------------------------------"
+
     if any(re.match('pdf.*',x) for x in params):
         params = sorted(params, key = lambda x: int(x.split('pdf')[-1]), reverse=False)
 
@@ -59,7 +91,7 @@ def plotPars(inputFile, pois=None, selectString='', maxPullsPerPlot=30, plotdir=
             os.system('mkdir '+distrdir)
             os.system('cp /afs/cern.ch/user/g/gpetrucc/php/index.php '+distrdir)
         for ext in ['png', 'pdf']:
-            c.SaveAs("{pdir}/{name}_postfit_{suffix}{channel}.{ext}".format(pdir=distrdir,name=name,suffix=suffix,channel=channel,ext=ext))
+            c.SaveAs("{pdir}/{name}_postfit_{suffix}_{channel}.{ext}".format(pdir=distrdir,name=name,suffix=suffix,channel=channel,ext=ext))
 
         if fitPull:
             tlatex = ROOT.TLatex(); tlatex.SetNDC(); 
@@ -90,13 +122,20 @@ def plotPars(inputFile, pois=None, selectString='', maxPullsPerPlot=30, plotdir=
             sortedpulls = []
             if 'pdf' in pois:
                 sortedpulls = sorted(pullSummaryMap.keys(), key=lambda k: int(k.split('pdf')[-1]))
+            # following condition should not happen (parameter is not centered around 0)
             elif 'masked' in pois:
                 keys = pullSummaryMap.keys()
-                keys_l = list(k for k in keys if 'left' in k)
-                keys_r = list(k for k in keys if 'right' in k)
-                norms_l = sorted(keys_l, key=lambda k: int(k.split('_')[-1]), reverse=False)
-                norms_r = sorted(keys_r, key=lambda k: int(k.split('_')[-1]), reverse=True)
-                sortedpulls = norms_r + norms_l
+                if analysis == "helicity":
+                    keys_l = list(k for k in keys if 'left' in k)
+                    keys_r = list(k for k in keys if 'right' in k)
+                    norms_l = sorted(keys_l, key=lambda k: int(k.split('_')[-1]), reverse=False)
+                    norms_r = sorted(keys_r, key=lambda k: int(k.split('_')[-1]), reverse=True)
+                    sortedpulls = norms_r + norms_l
+                else:
+                    # differential xsection: get ieta, ipt index and use them as keys to sort
+                    sortedpulls = sorted(keys, key = lambda x: get_ieta_ipt_from_process_name(x))
+            else:
+                sortedpulls = sorted(pullSummaryMap.keys(), key=lambda k: k[0])  # alphabetic order
             if len(sortedpulls)==0: break
 
             maxErr = 0;
@@ -115,8 +154,22 @@ def plotPars(inputFile, pois=None, selectString='', maxPullsPerPlot=30, plotdir=
                 pi += 1
                 nRemainingPulls -= 1
 
-            pull_rms .SetMarkerColor(46); pull_rms .SetLineColor(45); pull_rms .SetLineWidth(2); pull_rms .SetMarkerSize(1.0); pull_rms .SetMarkerStyle(21); 
-            pull_effsigma .SetMarkerColor(21); pull_effsigma .SetLineColor(24); pull_effsigma .SetLineWidth(2); pull_effsigma .SetMarkerSize(1.0); pull_effsigma .SetMarkerStyle(22)
+            #pull_rms .SetMarkerColor(46); pull_rms .SetLineColor(45); pull_rms .SetLineWidth(2); pull_rms .SetMarkerSize(1.0); pull_rms .SetMarkerStyle(21); 
+            #pull_effsigma .SetMarkerColor(21); pull_effsigma .SetLineColor(24); pull_effsigma .SetLineWidth(2); pull_effsigma .SetMarkerSize(1.0); pull_effsigma .SetMarkerStyle(22)
+
+            NmaxChar = 1
+            for name in sortedpulls:
+                NmaxChar = max(NmaxChar,len(name))
+            if NmaxChar >= 8:
+                hc.SetBottomMargin(0.2)  # hardcoded and not optimized I just looked at a random pull plot with the CMS_We_elescale and QCD scales parameters
+
+            pull_rms .SetMarkerColor(ROOT.kRed+2); 
+            pull_rms .SetLineColor(ROOT.kRed); pull_rms .SetLineWidth(3); 
+            pull_rms .SetMarkerSize(1.0);      pull_rms .SetMarkerStyle(21); 
+
+            pull_effsigma .SetMarkerColor(ROOT.kBlack); 
+            pull_effsigma .SetLineColor(ROOT.kGray+2);   pull_effsigma .SetLineWidth(3); 
+            pull_effsigma .SetMarkerSize(1.0);           pull_effsigma .SetMarkerStyle(22)
 
             pull_rms.SetLabelSize(pullLabelSize)
             pull_rms.LabelsOption("v")
@@ -137,7 +190,7 @@ def plotPars(inputFile, pois=None, selectString='', maxPullsPerPlot=30, plotdir=
             leg.Draw("same")
             param_group=pois.replace('.*','')
             for ext in ['png', 'pdf']:
-                hc.SaveAs("{pdir}/pullSummaryToys_{params}_{igroup}_{suffix}{c}.{ext}".format(pdir=plotdir,suffix=suffix,params=param_group,igroup=pullPlots,c=channel,ext=ext))
+                hc.SaveAs("{pdir}/pullSummaryToys_{params}_{igroup}_{suffix}_{c}.{ext}".format(pdir=plotdir,suffix=suffix,params=param_group,igroup=pullPlots,c=channel,ext=ext))
             pullPlots += 1
 
 
@@ -151,8 +204,10 @@ if __name__ == "__main__":
     from optparse import OptionParser
     parser = OptionParser(usage='%prog toys.root [options] ')
     parser.add_option(      '--parameters'  , dest='pois'     , default='pdf.*', type='string', help='comma separated list of regexp parameters to run. default is all parameters!')
+    parser.add_option(      '--exclude-param'  , dest='excludeParam'     , default=None, type='string', help='Work as --parameters, but matches will be excluded from the list of parameters (e.g., can exclude a given pdf, pmaskednorm to match only pmasked and so on)')
     parser.add_option(      '--pdir'        , dest='plotdir'  , default='./'   , type='string', help='directory to save the likelihood scans')
     parser.add_option(      '--suffix'      , dest='suffix'   , default=''     , type='string', help='suffix to give to the plot files')
+    parser.add_option('-a', '--analysis'    , dest='analysis' , default='helicity', type='string', help='Which analysis: helicity or diffXsec') 
 
     (options, args) = parser.parse_args()
 
@@ -160,6 +215,14 @@ if __name__ == "__main__":
         print "need toyfile as argument. Exit."
         exit(0)
 
+    if options.analysis not in ["helicity", "diffXsec"]:
+        print "Warning: analysis not recognized, must be either \"helicity\" or \"diffXsec\""
+        exit(0)
+ 
+    outname = options.plotdir
+    addStringToEnd(outname,"/",notAddIfEndswithMatch=True)
+    createPlotDirAndCopyPhp(outname)
+
     toyfile = args[0]
-    plotPars(toyfile,pois=options.pois,maxPullsPerPlot=30,plotdir=options.plotdir,suffix=options.suffix)
+    plotPars(toyfile,pois=options.pois,maxPullsPerPlot=30,plotdir=outname,suffix=options.suffix,analysis=options.analysis,excludeName=options.excludeParam)
 
