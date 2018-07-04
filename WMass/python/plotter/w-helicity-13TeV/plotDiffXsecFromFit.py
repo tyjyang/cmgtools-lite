18ae06a1ff36411f1176a30dbebd17377d5a5fe3
Marco Cipriani <cippy91@gmail.com>
2018-07-04 20:14:30 +0200

    many changes and improvements

--- WMass/python/plotter/w-helicity-13TeV/plotDiffXsecFromFit.py
+++ WMass/python/plotter/w-helicity-13TeV/plotDiffXsecFromFit.py
@@ -10,6 +10,9 @@ from make_diff_xsec_cards import getDiffXsecBinning
 from make_diff_xsec_cards import templateBinning
 from make_diff_xsec_cards import get_ieta_ipt_from_process_name
 
+from utilities import util
+utilities = util()
+
 import sys
 #sys.path.append(os.environ['CMSSW_BASE']+"/src/CMGTools/WMass/python/plotter/")
 sys.path.append(os.getcwd() + "/plotUtils/")
@@ -19,14 +22,31 @@ ROOT.gStyle.SetOptStat(0)
 #ROOT.gStyle.SetPadRightMargin(0.13)
 ROOT.gROOT.SetBatch()
 
+def getTH1fromTH2(h2D,h2Derr=0):
+    nbins = h2D.GetNbinsX() * h2D.GetNbinsY()
+    goodname = h2D.GetName()
+    h2D.SetName(goodname+"_unrollTo1D")
+    newh = ROOT.TH1D(goodname,h2D.GetTitle(),nbins,0.5,nbins+0.5)
+    if 'TH2' not in h2D.ClassName(): raise RuntimeError, "Calling getTH1fromTH2 on something that is not TH2"
+    for i in xrange(h2D.GetNbinsX()):
+        for j in xrange(h2D.GetNbinsY()):
+            bin = 1 + i + j*h2D.GetNbinsX()
+            newh.SetBinContent(bin,h2D.GetBinContent(i+1,j+1))
+            if h2Derr: newh.SetBinError(bin,h2Derr.GetBinContent(i+1,j+1))
+            else:      newh.SetBinError(bin,h2D.GetBinError(i+1,j+1))
+    return newh
+
+
 if __name__ == "__main__":
 
     from optparse import OptionParser
     parser = OptionParser(usage="%prog [options] fitresults.root")
+    parser.add_option('-t', '--type' , dest='type'  , default='hessian' , type='string', help='run the plot from which postfit? toys/scans/hessian')
     parser.add_option('-o','--outdir', dest='outdir', default='.', type='string', help='output directory to save things')
     parser.add_option('-c','--channel', dest='channel', default='el', type='string', help='Channel (el, mu)')
     parser.add_option('-C','--charge', dest='charge', default='plus,minus', type='string', help='Charges to consider')
     parser.add_option('-b','--etaPtbinning', dest='etaPtbinning', default='[-2.5,-1.566,-1.4442,0,1.4442,1.566,2.5]*[30,35,40,45]', type='string', help='eta-pt binning for templates (will have to implement reading it from file). Use -b file=<name> to read binning from file <name>')
+    parser.add_option('-n','--norm-width', dest='normWidth' , default=False , action='store_true',   help='if given, normalize cross section histograms dividing by bin width')
     (options, args) = parser.parse_args()
 
     if len(args) < 1:
@@ -37,9 +57,15 @@ if __name__ == "__main__":
     if channel not in ["el","mu"]:
         print "Error: unknown channel %s (select 'el' or 'mu')" % channel
         quit()
+        
+    if options.type not in ["toys", "hessian", "scans"]:
+        print 'ERROR: none of your types is supported. specify either "toys", "scans", or "hessian"'
+        sys.exit()
+
 
     outname = options.outdir
     addStringToEnd(outname,"/",notAddIfEndswithMatch=True)
+    outname = outname + options.type + "/"
     createPlotDirAndCopyPhp(outname)
 
     # get gen binning from file or directly from option
@@ -48,16 +74,26 @@ if __name__ == "__main__":
     etabinning = binning[0]
     ptbinning  = binning[1]
 
-    lepton = "electron" if channel == "el" else " muon"
     charges = options.charge.split(',')
 
     for charge in charges:
 
-        infile = ROOT.TFile(args[0], 'read')
         print ""
         print "==> RUNNING FOR CHARGE ",charge
         chs = '+' if charge == 'plus' else '-' 
         adjustSettings_CMS_lumi()
+        
+        if options.type == 'toys':
+            valuesAndErrors = utilities.getFromToys(args[0])
+        elif options.type == 'scans':
+            valuesAndErrors = utilities.getFromScans(args[0])
+        elif options.type == 'hessian':
+            valuesAndErrors = utilities.getFromHessian(args[0])
+
+        #channel check
+        channel = 'mu' if any(re.match(param,'.*_mu_group_.*') for param in valuesAndErrors.keys()) else 'el'
+        print "From the list of parameters it seems that you are plotting results for channel ",channel
+        lepton = "electron" if channel == "el" else " muon"
 
         hmu = ROOT.TH2F("hmu",'#mu: W{chs}'.format(chs=chs),len(etabinning)-1, array('d',etabinning), len(ptbinning)-1, array('d',ptbinning))
         hmu_err = ROOT.TH2F("hmu_err",'#mu uncertainty: W{chs}'.format(chs=chs),len(etabinning)-1, array('d',etabinning), len(ptbinning)-1, array('d',ptbinning))
@@ -72,36 +108,58 @@ if __name__ == "__main__":
         h_pmaskedexp_mu_err = ROOT.TH2F("h_pmaskedexp_mu_err",'pmaskedexp #mu uncertainty: W{chs}'.format(chs=chs),
                                         len(etabinning)-1, array('d',etabinning), len(ptbinning)-1, array('d',ptbinning))
 
-        tree = infile.Get("fitresults")
-        entries = tree.GetEntries()
-        leaves  = tree.GetListOfLeaves()
-        for entry in range(entries):  # actually it is just 1 entry 
-            tree.GetEntry(entry)
-            for p in leaves:
-                name = p.GetName()
-                if not "W{ch}".format(ch=charge) in name: continue
-                if any(x in name for x in ["_minosup", "_minosdown", "_gen"]): continue
-                # name should be like Wplus_el_ieta_4_ipt_1_Wplus_el_group_6_mu
-                etabinIndex,ptbinIndex = get_ieta_ipt_from_process_name(name)
-                    # eta and pt index start from 0, the corresponding histogram bin number is bin+1
+        for key,val in valuesAndErrors.iteritems():
+            if not "W{ch}".format(ch=charge) in key: continue 
+            etabinIndex,ptbinIndex = get_ieta_ipt_from_process_name(key)
+            mean = val[0]
+            err  = val[1]-mean
+            if "pmaskedexpnorm" in key:
+                h_pmaskedexpnorm_mu_err.SetBinContent(etabinIndex+1, ptbinIndex+1,err) 
+                h_pmaskedexpnorm_mu    .SetBinContent(etabinIndex+1, ptbinIndex+1,mean) 
+            elif "pmaskedexp" in key:
+                h_pmaskedexp_mu_err.SetBinContent(etabinIndex+1, ptbinIndex+1,err) 
+                h_pmaskedexp_mu    .SetBinContent(etabinIndex+1, ptbinIndex+1,mean) 
+            else: 
+                hmu_err.SetBinContent(etabinIndex+1, ptbinIndex+1,err)
+                hmu    .SetBinContent(etabinIndex+1, ptbinIndex+1,mean)
 
-                print "%s = %f" % (name, p.GetValue())
-                # could ask whether name.endswith("_mu"), but this would depend on the argument passed to --POIMode in text2tf.py
-                if "pmaskedexpnorm" in name: 
-                    if name.endswith("_err"): h_pmaskedexpnorm_mu_err.SetBinContent(etabinIndex+1, ptbinIndex+1,p.GetValue())
-                    else:                     h_pmaskedexpnorm_mu    .SetBinContent(etabinIndex+1, ptbinIndex+1,p.GetValue())
-                elif "pmaskedexp" in name:
-                    if name.endswith("_err"): h_pmaskedexp_mu_err.SetBinContent(etabinIndex+1, ptbinIndex+1,p.GetValue())
-                    else:                     h_pmaskedexp_mu    .SetBinContent(etabinIndex+1, ptbinIndex+1,p.GetValue())
-                else:
-                    if name.endswith("_err"): hmu_err.SetBinContent(etabinIndex+1, ptbinIndex+1,p.GetValue())
-                    else:                     hmu    .SetBinContent(etabinIndex+1, ptbinIndex+1,p.GetValue())
+        if options.normWidth:
+            h_pmaskedexpnorm_mu_err.Scale(1.,"width")
+            h_pmaskedexpnorm_mu.Scale(1.,"width")
+            h_pmaskedexp_mu_err.Scale(1.,"width")
+            h_pmaskedexp_mu.Scale(1.,"width")
+
+        # infile = ROOT.TFile(args[0], 'read')
+        # tree = infile.Get("fitresults")
+        # entries = tree.GetEntries()
+        # leaves  = tree.GetListOfLeaves()
+        # for entry in range(entries):  # actually it is just 1 entry 
+        #     tree.GetEntry(entry)
+        #     for p in leaves:
+        #         name = p.GetName()
+        #         if not "W{ch}".format(ch=charge) in name: continue
+        #         if any(x in name for x in ["_minos", "_In", "_gen"]): continue
+        #         # name should be like Wplus_el_ieta_4_ipt_1_Wplus_el_group_6_mu
+        #         etabinIndex,ptbinIndex = get_ieta_ipt_from_process_name(name)
+        #         # eta and pt index start from 0, the corresponding histogram bin number is bin+1
+
+        #         print "%s = %f" % (name, p.GetValue())
+        #         # could ask whether name.endswith("_mu"), but this would depend on the argument passed to --POIMode in text2tf.py
+        #         if "pmaskedexpnorm" in name: 
+        #             if name.endswith("_err"): h_pmaskedexpnorm_mu_err.SetBinContent(etabinIndex+1, ptbinIndex+1,p.GetValue())
+        #             else:                     h_pmaskedexpnorm_mu    .SetBinContent(etabinIndex+1, ptbinIndex+1,p.GetValue())
+        #         elif "pmaskedexp" in name:
+        #             if name.endswith("_err"): h_pmaskedexp_mu_err.SetBinContent(etabinIndex+1, ptbinIndex+1,p.GetValue())
+        #             else:                     h_pmaskedexp_mu    .SetBinContent(etabinIndex+1, ptbinIndex+1,p.GetValue())
+        #         else:
+        #             if name.endswith("_err"): hmu_err.SetBinContent(etabinIndex+1, ptbinIndex+1,p.GetValue())
+        #             else:                     hmu    .SetBinContent(etabinIndex+1, ptbinIndex+1,p.GetValue())
                 
         xaxisTitle = 'gen %s |#eta|' % lepton
         yaxisTitle = 'gen %s p_{T} [GeV]' % lepton
         #zaxisTitle = "#mu::%.3g,%.3g" % (hmu.GetMinimum(), hmu.GetMaximum())
 
-        zaxisTitle = "#mu::0.998,1.002"
+        zaxisTitle = "#mu::0.99,1.01"
         drawCorrelationPlot(hmu, 
                             xaxisTitle, yaxisTitle, zaxisTitle, 
                             hmu.GetName(),
@@ -158,4 +216,8 @@ if __name__ == "__main__":
                             h_pmaskedexpnorm_mu_relErr.GetName(),
                             "ForceTitle",outname,1,1,False,False,False,1,0.14,0.22)
 
-        infile.Close()
+        # now drawing a TH1 unrolling TH2
+        h1D_pmaskedexp = getTH1fromTH2(h_pmaskedexp_mu, h_pmaskedexp_mu_err)
+        
+
+        #infile.Close()
