3f1cf537b92a6eb67e9dc6195705e4ed4a8c69d0
Marco Cipriani <cippy91@gmail.com>
2018-07-10 18:48:30 +0200

    fixes

--- WMass/python/plotter/mergeToys.sh
+++ WMass/python/plotter/mergeToys.sh
@@ -4,10 +4,11 @@
 # merge file will be created in toys/<name>/ and will have <charge> in its name
 
 charge="plus"
-outdir="toys/diffXsec_2018_06_29_group10_absGenEta_moreEtaPtBin_new/"
+outdir="toys/diffXsec_mu_2018_07_04_group10_absGenEta/"
 toydir="${outdir}${charge}/"
 outname="toys_${charge}.root"
 sizemin="1000"
+maxFilePerHadd=100
 
 goodfiles_tmp=`ls -l ${toydir} | awk -v sizemin="$sizemin" '$5 > sizemin {print $9}' | grep root`
 ngood=`ls -l ${toydir} | awk -v sizemin="$sizemin" '$5 > sizemin {print $9}' | grep root | wc -l`
@@ -16,14 +17,17 @@ echo "There are ${ngood} files to merge (size > ${sizemin} bytes)"
 echo "--------------------------------------------------------------"
 
 goodfiles=""
+ifile=0
 for file in ${goodfiles_tmp};
 do
     tmpfile="${toydir}${file}"
     #echo "${tmpfile}"
     goodfiles="${goodfiles} ${tmpfile}"
+    let ifile++
 done
 
-hadd -k ${outdir}${outname} ${goodfiles}
+
+hadd -k -n ${maxFilePerHadd} ${outdir}${outname} ${goodfiles}
 
 echo "--------------------------------------------------------------"
 echo "THE END"
