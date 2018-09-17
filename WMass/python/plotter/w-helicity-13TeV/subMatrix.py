6855511b2d149952954f595c1fa2edd8cbb12e52
mdunser <marc.dunser@cern.ch>
2018-09-17 18:22:57 +0200

    added option --type to the script. supports toys/hessian.

--- WMass/python/plotter/w-helicity-13TeV/subMatrix.py
+++ WMass/python/plotter/w-helicity-13TeV/subMatrix.py
@@ -9,7 +9,7 @@ ROOT.gROOT.SetBatch(True)
 ## if output directory is given, it will save it there as pdf and png
 
 ## example:
-## python w-helicity-13TeV/subMatrix.py toys.root --params alph,muR,muF,.*Ybin.*2,pdf12,pdf56,pdf42 --outdir <output_directory>
+## python w-helicity-13TeV/subMatrix.py toys.root --params alph,muR,muF,.*Ybin.*2,pdf12,pdf56,pdf42 --outdir <output_directory> --type toys/hessian
 ## examples of common regexps:
 ## NORMs: 'norm_.*'
 ## PDFs: range [1,20]: '^pdf([1-9]|1[0-9])|20$'
@@ -21,7 +21,7 @@ def niceName(name):
     if '_Ybin' in name:
         nn  = '#mu: ' if '_mu_' in name else 'el: '
         nn += 'W+ ' if 'plus' in name else 'W- '
-        nn += 'left ' if 'left' in name else 'right '
+        nn += 'left ' if 'left' in name else 'right ' if 'right' in name else 'long '
         idx = -2 if 'masked' in name else -1
         nn += name.split('_')[idx]
         if 'pmaskedexp' in name: nn += ' #sigma'
@@ -46,8 +46,13 @@ if __name__ == "__main__":
     parser = OptionParser(usage='%prog workspace.root toys.root [options] ')
     parser.add_option('-o','--outdir', dest='outdir', default='', type='string', help='outdput directory to save the matrix')
     parser.add_option('-p','--params', dest='params', default='', type='string', help='parameters for which you want to show the correlation matrix. comma separated list of regexps')
+    parser.add_option('-t','--type'  , dest='type'  , default='toys', type='string', help='which type of input file: toys(default),scans, or hessian')
     parser.add_option(     '--suffix', dest='suffix', default='', type='string', help='suffix for the correlation matrix')
     (options, args) = parser.parse_args()
+
+    if not options.type in ['toys', 'scans', 'hessian']:
+        print 'the given type needs to be either "toys", "scans", or "hessian"!!'
+        sys.exit()
 
     if options.outdir:
         ROOT.gROOT.SetBatch()
@@ -58,48 +63,67 @@ if __name__ == "__main__":
     pois_regexps = list(options.params.split(','))
     print "Filtering POIs with the following regex: ",pois_regexps
 
-    ### GET LIST OF PARAMETERS THAT MATCH THE SPECIFIED OPTION IN THE TOYFILE
-    toyfile = ROOT.TFile(args[0], 'read')
-    _tree = toyfile.Get('fitresults')
-    lol = _tree.GetListOfLeaves()
-    params = []
+    params = []; indices = []
 
     ## directly store the mean and RMS into a dictionary
     fitvals = {}; fiterrs = {}
 
-    for l in lol:
-        ## skip a bunch of those we don't want
-        if '_err'   in l.GetName(): continue
-        if '_minos' in l.GetName(): continue
-        if '_gen'   in l.GetName(): continue
-        if '_In'    in l.GetName(): continue
-        for poi in pois_regexps:
-            if re.match(poi, l.GetName()):
-                ## draw the parameter into a histogram
-                _tree.Draw(l.GetName()+'>>h_'+l.GetName())
-                ## find that histogram and clone it
-                h = ROOT.gROOT.FindObject('h_'+l.GetName()).Clone()
-                ## store mean and rms into the dictionaries from before
-                fitvals[l.GetName()] = h.GetMean()
-                fiterrs[l.GetName()] = h.GetRMS()
-                ## also keep a list of the parameter names, for sorting
-                params.append(l.GetName())
-    
-    print "===> Build covariance matrix from this set of params: ", params
-
     cov = {}; corr = {}
 
+    ### GET LIST OF PARAMETERS THAT MATCH THE SPECIFIED OPTION IN THE TOYFILE
+    if options.type == 'toys':
+        toyfile = ROOT.TFile(args[0], 'read')
+        _tree = toyfile.Get('fitresults')
+        lol = _tree.GetListOfLeaves()
+
+        for l in lol:
+            ## skip a bunch of those we don't want
+            if '_err'   in l.GetName(): continue
+            if '_minos' in l.GetName(): continue
+            if '_gen'   in l.GetName(): continue
+            if '_In'    in l.GetName(): continue
+            for poi in pois_regexps:
+                if re.match(poi, l.GetName()):
+                    ## draw the parameter into a histogram
+                    _tree.Draw(l.GetName()+'>>h_'+l.GetName())
+                    ## find that histogram and clone it
+                    h = ROOT.gROOT.FindObject('h_'+l.GetName()).Clone()
+                    ## store mean and rms into the dictionaries from before
+                    fitvals[l.GetName()] = h.GetMean()
+                    fiterrs[l.GetName()] = h.GetRMS()
+                    ## also keep a list of the parameter names, for sorting
+                    params.append(l.GetName())
+
+    elif options.type == 'hessian':
+        hessfile = ROOT.TFile(args[0],'read')
+        corrmatrix = hessfile.Get('correlation_matrix_channelpmaskedexpnorm')
+        covmatrix  = hessfile.Get('covariance_matrix_channelpmaskedexpnorm')
+        for ib in range(1+corrmatrix.GetNbinsX()+1):
+            for poi in pois_regexps:
+                if re.match(poi, corrmatrix.GetXaxis().GetBinLabel(ib)):
+                    ## store mean and rms into the dictionaries from before
+                    ## also keep a list of the parameter names, for sorting
+                    params .append(corrmatrix.GetXaxis().GetBinLabel(ib))
+                    indices.append(ib)
+            
     ## construct the covariances and the correlations in one go.
-    for p1 in params:
-        for p2 in params:
-            var = '({x}-{x0})*({y}-{y0})'.format(x=p1,x0=fitvals[p1],y=p2,y0=fitvals[p2])
-            _tree.Draw('{var}>>h_{x}_{y}'.format(var=var,x=p1,y=p2))
-            h = ROOT.gROOT.FindObject('h_{x}_{y}'.format(x=p1,y=p2)).Clone()
-            cov [(p1,p2)] = h.GetMean()
-            corr[(p1,p2)] = cov[(p1,p2)]/(fiterrs[p1]*fiterrs[p2])
+    for ip1, p1 in enumerate(params):
+        for ip2, p2 in enumerate(params):
+            if options.type == 'toys':
+                var = '({x}-{x0})*({y}-{y0})'.format(x=p1,x0=fitvals[p1],y=p2,y0=fitvals[p2])
+                _tree.Draw('{var}>>h_{x}_{y}'.format(var=var,x=p1,y=p2))
+                h = ROOT.gROOT.FindObject('h_{x}_{y}'.format(x=p1,y=p2)).Clone()
+                cov [(p1,p2)] = h.GetMean()
+                corr[(p1,p2)] = cov[(p1,p2)]/(fiterrs[p1]*fiterrs[p2])
+            elif options.type == 'hessian':
+                cov [(p1,p2)] = covmatrix .GetBinContent(indices[ip1],indices[ip2])
+                corr[(p1,p2)] = corrmatrix.GetBinContent(indices[ip1],indices[ip2])
+        
+
+    print "===> Build covariance matrix from this set of params: ", params
 
     ## sort the floatParams. alphabetically, except for pdfs, which are sorted by number
-    params = sorted(params, key= lambda x: int(x.split('_')[-2]) if '_Ybin_' in x else 0)
+    params = sorted(params, key= lambda x: int(x.split('_')[-1]) if '_Ybin_' in x else 0)
     params = sorted(params, key= lambda x: int(x.replace('pdf','')) if 'pdf' in x else 0)
             
     print "sorted params = ", params
