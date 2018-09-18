3a4348226156b78d5711c2c67e226f782592ca9f
mdunser <marc.dunser@cern.ch>
2018-09-18 13:45:40 +0200

    this script can now deal with multiple processes in the rootfile

--- WMass/python/plotter/w-helicity-13TeV/checkEtaAsymmetry.py
+++ WMass/python/plotter/w-helicity-13TeV/checkEtaAsymmetry.py
@@ -8,13 +8,33 @@ if __name__ == "__main__":
     from optparse import OptionParser
     parser = OptionParser(usage='%prog [options] ')
     parser.add_option('-i', '--infile'         , dest='infile'     , default=''      , type='string', help='input file with the plot and ratio')
+    parser.add_option('-v', '--var'            , dest='var'        , default='eta'   , type='string', help='name of the plotted variable')
     (options, args) = parser.parse_args()
 
-    var = os.path.basename(options.infile).split('.')[0]
+    #var = os.path.basename(options.infile).split('.')[0]
 
     inf = ROOT.TFile(options.infile,'read')
-    bkg  = inf.Get(var+'_Z')
-    data = inf.Get(var+'_data')
+
+    sig, bkg = 0, 0
+    lok = inf.GetListOfKeys()
+    for k in lok:
+        if not options.var in k.GetName(): 
+            continue
+        if options.var in k.GetName() and '_signal' in k.GetName():
+            sig  = inf.Get(options.var+'_signal')
+        if options.var in k.GetName() and '_background' in k.GetName():
+            bkg  = inf.Get(options.var+'_background')
+        if options.var in k.GetName() and '_data' in k.GetName():
+            data = inf.Get(options.var+'_data')
+        
+    if sig and bkg:
+        bkg.Add(sig)
+    elif bkg and not sig:
+        bkg = bkg
+    elif not bkg and sig:
+        bkg = sig
+    else:
+        print 'something went massively wrong!'
 
     ratio = data.Clone('ratio')
     ratio.Sumw2()
@@ -62,6 +82,12 @@ if __name__ == "__main__":
     leg.AddEntry(histright, 'positive eta', 'pl')
     leg.Draw('same')
 
-    canv.SaveAs(os.path.dirname(options.infile)+'/'+var+'_asymmetry.pdf')
-    canv.SaveAs(os.path.dirname(options.infile)+'/'+var+'_asymmetry.png')
+    line = ROOT.TLine()
+    line.SetLineStyle(3)
+    line.SetLineWidth(2)
+    line.SetLineColor(ROOT.kBlack)
+    line.DrawLine(binedges[0], 1., binedges[-1], 1.)
+
+    canv.SaveAs(os.path.dirname(options.infile)+'/'+options.var+'_asymmetry.pdf')
+    canv.SaveAs(os.path.dirname(options.infile)+'/'+options.var+'_asymmetry.png')
 
