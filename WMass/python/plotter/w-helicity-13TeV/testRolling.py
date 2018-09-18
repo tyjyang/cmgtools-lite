4e756cd0c08747364db2ecfa6cf343b7bcd1affe
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2018-09-18 17:41:56 +0200

    add possibility to scale by the postfit results

--- WMass/python/plotter/w-helicity-13TeV/testRolling.py
+++ WMass/python/plotter/w-helicity-13TeV/testRolling.py
@@ -5,6 +5,8 @@
 import ROOT, os
 from array import array
 
+import utilities
+utilities = utilities.util()
 
 def getbinning(splitline):
     bins = splitline[5]
@@ -41,6 +43,7 @@ def unroll2Dto1D(h):
     goodname = h.GetName()
     h.SetName(goodname+"_oldbinning")
     newh = ROOT.TH1D(goodname,h.GetTitle(),nbins,0.5,nbins+0.5)
+    newh.Sumw2()
     if 'TH2' not in h.ClassName(): raise RuntimeError, "Calling rebin2Dto1D on something that is not TH2"
     for i in xrange(h.GetNbinsX()):
         for j in xrange(h.GetNbinsY()):
@@ -72,6 +75,66 @@ def dressed2D(h1d,binning,name,title=''):
     h2_backrolled_1 .GetZaxis().SetRangeUser(0.01*h2_backrolled_1.GetMaximum(),1.1*h2_backrolled_1.GetMaximum())
     return h2_backrolled_1
 
+def doRatioHists(data,total,maxRange,fixRange=False,ylabel="Data/pred.",yndiv=505,doWide=False,showStatTotLegend=False,textSize=0.035):
+    ratio = data.Clone("data_div"); 
+    ratio.Sumw2()
+    ratio.Divide(total)
+
+    unity = total.Clone("sim_div");
+    rmin, rmax =  1,1
+    for b in xrange(1,unity.GetNbinsX()+1):
+        e,n = unity.GetBinError(b), unity.GetBinContent(b)
+        unity.SetBinContent(b, 1 if n > 0 else 0)
+        unity.SetBinError(b, 0)
+        rmin = min([ rmin, 1-2*e/n if n > 0 else 1])
+        rmax = max([ rmax, 1+2*e/n if n > 0 else 1])
+    for b in xrange(1,unity.GetNbinsX()+1):
+        if ratio.GetBinContent(b) == 0: continue
+        rmin = min([ rmin, ratio.GetBinContent(b) - 2*ratio.GetBinError(b) ]) 
+        rmax = max([ rmax, ratio.GetBinContent(b) + 2*ratio.GetBinError(b) ])  
+    if rmin < maxRange[0] or fixRange: rmin = maxRange[0]; 
+    if rmax > maxRange[1] or fixRange: rmax = maxRange[1];
+    if (rmax > 3 and rmax <= 3.4): rmax = 3.4
+    if (rmax > 2 and rmax <= 2.4): rmax = 2.4
+    unity.SetFillStyle(1001);
+    unity.SetFillColor(ROOT.kCyan);
+    unity.SetMarkerStyle(1);
+    unity.SetMarkerColor(ROOT.kCyan);
+    ROOT.gStyle.SetErrorX(0.5);
+    unity.Draw("AXIS");
+    unity.GetYaxis().SetRangeUser(rmin,rmax);
+    unity.GetXaxis().SetTitleFont(42)
+    unity.GetXaxis().SetTitleSize(0.14)
+    unity.GetXaxis().SetTitleOffset(0.9)
+    unity.GetXaxis().SetLabelFont(42)
+    unity.GetXaxis().SetLabelSize(0.1)
+    unity.GetXaxis().SetLabelOffset(0.007)
+    unity.GetYaxis().SetNdivisions(505)
+    unity.GetYaxis().SetTitleFont(42)
+    unity.GetYaxis().SetTitleSize(0.14)
+    offset = 0.32 if doWide else 0.62
+    unity.GetYaxis().SetTitleOffset(offset)
+    unity.GetYaxis().SetLabelFont(42)
+    unity.GetYaxis().SetLabelSize(0.11)
+    unity.GetYaxis().SetLabelOffset(0.007)
+    unity.GetYaxis().SetDecimals(True) 
+    unity.GetYaxis().SetTitle(ylabel)
+    total.GetXaxis().SetLabelOffset(999) ## send them away
+    total.GetXaxis().SetTitleOffset(999) ## in outer space
+    total.GetYaxis().SetTitleSize(0.06)
+    total.GetYaxis().SetTitleOffset(0.75 if doWide else 1.48)
+    total.GetYaxis().SetLabelSize(0.05)
+    total.GetYaxis().SetLabelOffset(0.007)
+    #$ROOT.gStyle.SetErrorX(0.0);
+    line = ROOT.TLine(unity.GetXaxis().GetXmin(),1,unity.GetXaxis().GetXmax(),1)
+    line.SetLineWidth(2);
+    line.SetLineColor(58);
+    line.Draw("L")
+    ratio.Draw("E SAME" if ratio.ClassName() != "TGraphAsymmErrors" else "PZ SAME");
+    return (ratio, unity, line)
+
+
+
 ROOT.gROOT.SetBatch()
 
 if __name__ == "__main__":
@@ -80,19 +143,24 @@ if __name__ == "__main__":
     parser.add_option('-o','--outdir', dest='outdir', default='.', type='string', help='output directory to save the matrix')
     parser.add_option(     '--noplot', dest="noplot", default=False, action='store_true', help="Do not plot templates (but you can still save them in a root file with option -s)");
     parser.add_option('-s','--save', dest='outfile_templates', default='templates_2D', type='string', help='pass name of output file to save 2D histograms (charge is automatically appended before extension). No need to specify extension, .root is automatically addedx')
+    parser.add_option(     '--fitres', dest='fitresult', default=None, type='string', help='if given, uses the contents of fitresults to scale the templates to post-fit result')
     groupJobs=5 # used in make_helicity_cards.py
-    ybinfile = args[0]+'/binningYW.txt'
-    ybinfile = open(ybinfile, 'r')
-    ybins = eval(ybinfile.read())
-    ybinfile.close()
-    nYbins=len(ybins)
 
     (options, args) = parser.parse_args()
     if len(args) < 2:
         parser.print_usage()
         quit()
     channel = args[1]
+
+    ybinfile = args[0]+'/binningYW.txt'
+    ybinfile = open(ybinfile, 'r')
+    ybins = eval(ybinfile.read())
+    ybinfile.close()
     
+    if options.fitresult:
+        print "Scaling the templates to the post-fit results in: ",options.fitresult
+        valuesAndErrors = utilities.getFromHessian(options.fitresult)
+
     outname = options.outdir
     if not os.path.exists(outname):
         os.system("mkdir -p "+outname)
@@ -115,21 +183,26 @@ if __name__ == "__main__":
         print "Will save 2D templates in file --> " + full_outfileName
 
         # doing signal
+        total_sig = {}
+        canv = ROOT.TCanvas()
         for pol in ['right', 'left', 'long']:
             print "\tPOLARIZATION ",pol
+            chpol = '{ch}_{pol}'.format(ch=charge,pol=pol)
+            nYbins=len(ybins[chpol])-1
+            #for ybin in range(1):
             for ybin in range(nYbins): 
 
                 group = ybin/groupJobs + 1
                 jobind = min(groupJobs * group - 1,nYbins-1)
                 line = ybin%groupJobs
                 
+
                 jobsdir = args[0]+'/jobs/'
                 jobfile_name = 'W{ch}_{pol}_{flav}_Ybin_{b}.sh'.format(ch=charge,pol=pol,flav=channel,b=jobind)
                 tmp_jobfile = open(jobsdir+jobfile_name, 'r')
                 lineno = (-groupJobs+line)*2 + 1 # white lines
                 if ybin==nYbins-1: lineno = -1 # not general hack!!!
                 tmp_line = tmp_jobfile.readlines()[lineno].split()
-                print "tmp_line = ",tmp_line
                 ymin = list(i for i in tmp_line if '(genw_y)>' in i)[0].replace('\'','').split('>')[-1]
                 ymax = list(i for i in tmp_line if '(genw_y)<' in i)[0].replace('\'','').split('<')[-1]
                 
@@ -140,21 +213,41 @@ if __name__ == "__main__":
                 name2D = 'W{ch}_{pol}_W{ch}_{pol}_{flav}_Ybin_{ybin}'.format(ch=charge,pol=pol,flav=channel,ybin=ybin)
                 title2D = 'W{ch} {pol} : |Y_{{W}}| #in [{ymin},{ymax}]'.format(ymin=ymin,ymax=ymax,pol=pol,ybin=ybin,ch=chs)
                 h2_backrolled_1 = dressed2D(h1_1,binning,name2D,title2D)
-                canv = ROOT.TCanvas()
+                if options.fitresult:
+                    scale = valuesAndErrors[name2D+'_mu']
+                    print "scaling for {h} = {val:.3f} +/- {err:.3f} ".format(h=name2D,val=scale[0],err=scale[1]-scale[0])
+                    h2_backrolled_1.Scale(scale[0])
+                if ybin==0:
+                    total_sig["W"+chpol] = h2_backrolled_1.Clone('W{ch}_{pol}_{flav}'.format(ch=charge,pol=pol,flav=channel))
+                else:
+                    total_sig["W"+chpol].Add(h2_backrolled_1)
+                total_sig["W"+chpol].SetDirectory(None)
+
                 h2_backrolled_1.Draw('colz')
                 h2_backrolled_1.Write(name2D)
                 if not options.noplot:
                     for ext in ['pdf', 'png']:
                         canv.SaveAs('{odir}/W{ch}_{pol}_W{ch}_{flav}_Ybin_{ybin}_PFMT40_absY.{ext}'.format(odir=outname,ch=charge,flav=channel,pol=pol,ybin=ybin,ext=ext))
+            total_sig["W"+chpol].Draw('colz')
+            total_sig["W"+chpol].Write(total_sig["W"+chpol].GetName())
+            if not options.noplot:
+                for ext in ['pdf', 'png']:
+                    canv.SaveAs('{odir}/W{ch}_{pol}_{flav}_TOTALSIG_PFMT40_absY.{ext}'.format(odir=outname,ch=charge,flav=channel,pol=pol,ext=ext))
 
         # do backgrounds now
-        procs=["Flips","Z","Top","DiBosons","TauDecaysW","data_fakes","W{ch}_long".format(ch=charge)]
-        titles=["charge flips","DY","Top","di-bosons","W#to#tau#nu","QCD","W{ch}_long".format(ch=charge)]
+        procs=["Flips","Z","Top","DiBosons","TauDecaysW","data_fakes","W{ch}_long".format(ch=charge),"data_obs"]
+        titles=["charge flips","DY","Top","di-bosons","W#to#tau#nu","QCD","W{ch}_long".format(ch=charge),"data"]
+        bkg_and_data = {}
         for i,p in enumerate(procs):
             h1_1 = infile.Get('x_{p}'.format(p=p))
             if not h1_1: continue # muons don't have Flips components
             h2_backrolled_1 = dressed2D(h1_1,binning,p,titles[i])
-            canv = ROOT.TCanvas()
+            if options.fitresult:
+                scale = valuesAndErrors[name2D+'_mu']
+                print "scaling for {h} = {val:.3f} +/- {err:.3f} ".format(h=name2D,val=scale[0],err=scale[1]-scale[0])
+                h2_backrolled_1.Scale(scale[0])
+            bkg_and_data[p] = h2_backrolled_1
+            bkg_and_data[p].SetDirectory(None)
             h2_backrolled_1.Draw('colz')
             h2_backrolled_1.Write(str(p))
             if not options.noplot:
@@ -162,7 +255,74 @@ if __name__ == "__main__":
                     canv.SaveAs('{odir}/{proc}_{ch}_{flav}_PFMT40_absY.{ext}'.format(odir=outname,proc=p,ch=charge,flav=channel,ext=ext))
             
         outfile.Close()
+    
+        # now draw the 1D projections
+        all_procs = total_sig.copy()
+        all_procs.update(bkg_and_data)
+        print all_procs
+        print total_sig,"   bkgs = ",bkg_and_data
 
+     
+
+        colors = {'Wplus_long' : ROOT.kGray+1,   'Wplus_left' : ROOT.kBlue-1,   'Wplus_right' : ROOT.kGreen+1,
+                  'Wminus_long': ROOT.kYellow+1, 'Wminus_left': ROOT.kViolet-1, 'Wminus_right': ROOT.kAzure+1,
+                  'Top'        : ROOT.kGreen+2,  'DiBosons'   : ROOT.kViolet+2, 'TauDecaysW'  : ROOT.kPink,       
+                  'Z'          : ROOT.kAzure+2,  'Flips'      : ROOT.kGray+1,   'data_fakes'  : ROOT.kGray+2 }
+                  
+
+        projection='X'
+        legWidth=0.18; textSize=0.035
+        (x1,y1,x2,y2) = (.85-legWidth, .70, .90, .91)
+        leg = ROOT.TLegend(x1,y1,x2,y2)
+     
+        hdata = all_procs['data_obs'].ProjectionX().Clone('x_total') if projection=='X' else all_procs['data_obs'].ProjectionY().Clone('x_total')
+        htot = hdata.Clone('x_data_obs')
+        htot.Reset()
+        stack = ROOT.THStack("x_stack","")
+        for proc in all_procs:
+            if proc=='data_obs': continue
+            proj1d = all_procs[proc].ProjectionX() if  projection=='X' else all_procs[proc].ProjectionY()
+            #keycol = proc  if '_'+channel not in proc else "_".join(proc.split('_')[:-1])
+            proj1d.SetFillColor(colors[proc])
+            htot.Add(proj1d)
+            stack.Add(proj1d)
+            leg.AddEntry(proj1d,proc,'F')
+        
+        doRatio=True
+        htot.GetYaxis().SetRangeUser(0, 1.8*max(htot.GetMaximum(), hdata.GetMaximum()))
+     
+
+        ## Prepare split screen
+        c1 = ROOT.TCanvas("c1", "c1", 600, 750); c1.Draw()
+        c1.SetWindowSize(600 + (600 - c1.GetWw()), (750 + (750 - c1.GetWh())));
+        p1 = ROOT.TPad("pad1","pad1",0,0.29,1,0.99);
+        p1.SetBottomMargin(0.03);
+        p1.Draw();
+        p2 = ROOT.TPad("pad2","pad2",0,0,1,0.31);
+        p2.SetTopMargin(0);
+        p2.SetBottomMargin(0.3);
+        p2.SetFillStyle(0);
+        p2.Draw();
+        p1.cd();
+        ## Draw absolute prediction in top frame
+        htot.Draw("HIST")
+        #htot.SetLabelOffset(9999.0);
+        #htot.SetTitleOffset(9999.0);
+        stack.Draw("HIST F SAME")
+        hdata.SetMarkerColor(ROOT.kBlack)
+        hdata.SetMarkerStyle(ROOT.kFullCircle)
+        hdata.Draw("E SAME")
+        htot.Draw("AXIS SAME")
+        leg.Draw()
+
+        p2.cd()
+        rdata,rnorm,rline = doRatioHists(hdata, htot, maxRange=[0.90,1.10], fixRange=True)
+        c1.cd()
+        for ext in ['pdf', 'png']:
+            c1.SaveAs('{odir}/projection{proj}_{ch}_{flav}_PFMT40_absY.{ext}'.format(odir=outname,proj=projection,ch=charge,flav=channel,ext=ext))
+        
+        
+    
     ## canv.Divide(1,2)
     ## canv.cd(1)
     ## h2.Draw('colz')
