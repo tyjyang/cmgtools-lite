e7a647e1c98bc3807d77bddf3edd3aa2e0515c8c
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2018-10-12 15:20:50 +0200

    replace the EE clustering SF with the L1 prefiring eff. Use the free function for now

--- WMass/python/plotter/w-helicity-13TeV/wmass_e/make_cards_el.py
+++ WMass/python/plotter/w-helicity-13TeV/wmass_e/make_cards_el.py
@@ -18,7 +18,7 @@ TREEPATH="/eos/cms/store/cmst3/group/wmass/w-helicity-13TeV/trees/TREES_electron
 QUEUE=str(options.queue)
 VAR="\"ptElFull(LepGood1_calPt,LepGood1_eta):LepGood1_eta\""
 BINNING="\"[-2.5,-2.4,-2.3,-2.2,-2.1,-2.0,-1.9,-1.8,-1.7,-1.6,-1.566,-1.4442,-1.4,-1.3,-1.2,-1.1,-1.0,-0.9,-0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.1,0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.4442,1.566,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,2.4,2.5]*[30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45]\""
-WEIGHTSTRING="\'puw2016_nTrueInt_36fb(nTrueInt)*lepSF(LepGood1_pdgId,LepGood1_pt,LepGood1_eta,LepGood1_SF1,LepGood1_SF2,LepGood1_SF3,LepGood1_SF4)\' "
+WEIGHTSTRING="\'puw2016_nTrueInt_36fb(nTrueInt)*lepSF(LepGood1_pdgId,LepGood1_pt,LepGood1_eta,LepGood1_SF1,LepGood1_SF2,LepGood1_SF3)\' "
 LUMI=35.9
 
 OUTDIR="helicity_%s" % datetime.now().strftime("%Y_%m_%d")
--- WMass/python/plotter/w-helicity-13TeV/wmass_e/wmass_plots.py
+++ WMass/python/plotter/w-helicity-13TeV/wmass_e/wmass_plots.py
@@ -40,7 +40,7 @@ def base(selection,useSkim=True):
 
     if selection=='wenu':
         GO="%s w-helicity-13TeV/wmass_e/mca-80X-wenu-simpleplots.txt w-helicity-13TeV/wmass_e/wenu_80X.txt "%CORE
-        GO="%s -W 'puw2016_nTrueInt_36fb(nTrueInt)*lepSF(LepGood1_pdgId,LepGood1_pt,LepGood1_eta,LepGood1_SF1,LepGood1_SF2,LepGood1_SF3,LepGood1_SF4)'"%GO
+        GO="%s -W 'puw2016_nTrueInt_36fb(nTrueInt)*lepSF(LepGood1_pdgId,LepGood1_pt,LepGood1_eta,LepGood1_SF1,LepGood1_SF2,LepGood1_SF3)'"%GO
         if dowhat in ["plots","ntuple"]: GO+=" w-helicity-13TeV/wmass_e/wenu_plots.txt "
     elif selection=='wgen':
         GO="%s w-helicity-13TeV/wmass_e/mca-80X-wenu-helicity.txt w-helicity-13TeV/wmass_e/wenu_80X.txt "%CORE
@@ -51,7 +51,8 @@ def base(selection,useSkim=True):
     elif selection=='zee':
         GO="%s w-helicity-13TeV/wmass_e/mca-80X-zee.txt w-helicity-13TeV/wmass_e/zee.txt --fitData --flp Z "%CORE
         # GO="%s -W 'puw2016_nTrueInt_36fb(nTrueInt)*eleSF_HLT_2l(LepGood1_matchedTrgObjElePt,LepGood1_pt,LepGood1_eta,LepGood2_matchedTrgObjElePt,LepGood2_pt,LepGood2_eta)*eleSF_GSFReco(LepGood1_pt,LepGood1_eta)*eleSF_FullID(LepGood1_pt,LepGood1_eta)*eleSF_GSFReco(LepGood2_pt,LepGood2_eta)*eleSF_FullID(LepGood2_pt,LepGood2_eta)*eleSF_Clustering(LepGood1_pt,LepGood1_eta)*eleSF_Clustering(LepGood2_pt,LepGood2_eta)' --sp 'Z' "%GO
-        GO="%s -W 'puw2016_nTrueInt_36fb(nTrueInt)*eleSF_HLT_2lfriends(LepGood1_matchedTrgObjElePt,LepGood1_SF1,LepGood2_matchedTrgObjElePt,LepGood2_SF1)*LepGood1_SF2*LepGood1_SF3*LepGood1_SF4*LepGood2_SF2*LepGood2_SF3*LepGood2_SF4' --sp 'Z' "%GO
+        # GO="%s -W 'puw2016_nTrueInt_36fb(nTrueInt)*eleSF_HLT_2lfriends(LepGood1_matchedTrgObjElePt,LepGood1_SF1,LepGood2_matchedTrgObjElePt,LepGood2_SF1)*LepGood1_SF2*LepGood1_SF3*LepGood1_SF4*LepGood2_SF2*LepGood2_SF3*LepGood2_SF4' --sp 'Z' "%GO
+        GO="%s -W 'puw2016_nTrueInt_36fb(nTrueInt)*eleSF_HLT_2lfriends(LepGood1_matchedTrgObjElePt,LepGood1_SF1,LepGood2_matchedTrgObjElePt,LepGood2_SF1)*LepGood1_SF2*LepGood1_SF3*LepGood2_SF2*LepGood2_SF3*eleSF_L1Eff_2l(LepGood1_pt,LepGood1_eta,LepGood2_pt,LepGood2_eta)' --sp 'Z' "%GO
         if dowhat in ["plots","ntuple"]: GO+=" w-helicity-13TeV/wmass_e/zee_plots.txt "
     else:
         raise RuntimeError, 'Unknown selection'
