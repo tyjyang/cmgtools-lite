6a813921f5d12cf9e77c63461d775c2b50a4c7b7
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2020-01-10 22:20:55 +0100

    add 94X miniAOD v3

---
+++ RootTools/python/samples/samples_13TeV_RunIISummer16MiniAODv3.py
@@ -0,0 +1,36 @@
+import PhysicsTools.HeppyCore.framework.config as cfg
+import os
+
+#####COMPONENT CREATOR
+
+from CMGTools.RootTools.samples.ComponentCreator import ComponentCreator
+kreator = ComponentCreator()
+
+
+# TTbar cross section: NNLO, https://twiki.cern.ch/twiki/bin/view/LHCPhysics/TtbarNNLO (172.5)
+ZJToMuMu_powhegMiNNLO_pythia8_testProd        = kreator.makeMCComponent("ZJToMuMu_powhegMiNNLO_pythia8_testProd"       , "/ZJToMuMu_TuneCUETP8M1_13TeV-powheg-MiNNLO-pythia8/RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v1/MINIAODSIM"       , "CMS", ".*root", 2008.4)
+ZJToMuMu_powhegMiNNLO_pythia8_photos_testProd = kreator.makeMCComponent("ZJToMuMu_powhegMiNNLO_pythia8_photos_testProd", "/ZJToMuMu_TuneCUETP8M1_13TeV-powheg-MiNNLO-pythia8-photos/RunIISummer16MiniAODv3-PUMoriond17_94X_mcRun2_asymptotic_v3-v1/MINIAODSIM", "CMS", ".*root", 2008.4)
+
+### ----------------------------- summary ----------------------------------------
+
+mcSamples = [ZJToMuMu_powhegMiNNLO_pythia8_testProd, ZJToMuMu_powhegMiNNLO_pythia8_photos_testProd ]
+
+samples = mcSamples
+
+### ---------------------------------------------------------------------
+
+from CMGTools.TTHAnalysis.setup.Efficiencies import *
+dataDir = "$CMSSW_BASE/src/CMGTools/TTHAnalysis/data"
+
+#Define splitting
+for comp in mcSamples:
+    comp.isMC = True
+    comp.isData = False
+    comp.splitFactor = 250 #  if comp.name in [ "WJets", "DY3JetsM50", "DY4JetsM50","W1Jets","W2Jets","W3Jets","W4Jets","TTJetsHad" ] else 100
+    comp.puFileMC=dataDir+"/puProfile_Summer12_53X.root"
+    comp.puFileData=dataDir+"/puProfile_Data12.root"
+    comp.efficiency = eff2012
+
+if __name__ == "__main__":
+    from CMGTools.RootTools.samples.tools import runMain
+    runMain(samples,localobjs=locals())
