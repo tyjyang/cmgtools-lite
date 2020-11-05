6dfa828747981f59b4e24296f7ea43c6a259cd6d
cippy <marco.cipriani@cern.ch>
2020-11-05 23:09:02 +0100

    deleting useless stuff, committed by mistake

--- WMass/python/postprocessing/log_friends_W_94X_v1/dummy_exec.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /afs/cern.ch/work/m/mciprian/w_mass_analysis/heppy/rel_slc7/CMSSW_9_4_12/src/CMGTools/WMass/python/postprocessing
-eval `scramv1 runtime -sh` 
-echo i will run the following command
-echo python $*
-python $*
