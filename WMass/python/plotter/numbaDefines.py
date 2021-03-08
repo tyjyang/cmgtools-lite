9f02fc902049eb92a5c5bd0e250582472b5fa9c1
Kenneth Long <kdlong@hep.wisc.edu>
2021-03-08 23:26:46 +0100

    Use numba to define the prefsr indices

--- WMass/python/plotter/numbaDefines.py
+++ WMass/python/plotter/numbaDefines.py
@@ -17,3 +17,30 @@ def correctN3LL_Wp(ptW):
     if corrbin[0] >= len(correctionsWp):
         return 1.
     return correctionsWp[corrbin][0]
+
+@ROOT.Numba.Declare(["RVec<int>","RVec<int>","RVec<int>","RVec<int>", "RVec<float>", ], "RVec<bool>")
+def prefsrLeptons(status, statusFlags, pdgId, motherIdx, pts):
+    leptons = (np.abs(pdgId) >= 11) & (np.abs(pdgId) <= 14) & (motherIdx >= 0)
+    status746 = status == 746
+    status23 = status == 23
+    motherV = (pdgId[motherIdx] == 23) | (np.abs(pdgId[motherIdx]) == 24)
+    fromHardProcess = ((statusFlags >> 8 ) & 1).astype(np.bool_)
+
+    # Some leptons in MadGraph have no W/Z in the history, but have status 23
+    others = leptons & (motherV | status23)
+    if np.count_nonzero(others) > 2:
+        others = others & fromHardProcess
+    
+    # If there are status = 746 leptons, they came from photos and are pre-FSR
+    photos = leptons & status746
+    photos = status746
+    nphotos = np.count_nonzero(photos)
+    nothers = np.count_nonzero(others)
+
+    if nphotos == 2:
+        return photos
+    elif nphotos == 1 and nothers == 1:
+        return photos | others
+
+    return others
+
