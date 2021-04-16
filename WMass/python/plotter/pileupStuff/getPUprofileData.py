d06d61f438fc8bd3984953d7e60e8db15b4a08d5
Marco Cipriani <marco.cipriani@cern.ch>
2021-04-16 20:05:19 +0200

    update

---
+++ WMass/python/plotter/pileupStuff/getPUprofileData.py
@@ -0,0 +1,21 @@
+#!/bin/env python
+
+# run brilcalc to get pu profiles per era (for the inclusive one you can just run 1 command and be fine)
+# it also plots the profiles
+# requires cmssw-cc7 singularity environment to run, and cmsenv from a release, then can be used from anywhere
+# should call with python3
+
+import os, re, array, math
+import argparse
+## safe batch mode
+import sys
+
+sys.path.append(os.getcwd() + "/lumiStuff/")
+from runPerEra import lumiForEra_UL_new as lfe
+
+pileupFile = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/PileUp/pileup_latest.txt"
+eras = list(lfe.keys())
+
+for era in eras:
+    cmd = f"pileupCalc.py -i lumiStuff/NanoAOD_json_Run2016{era}_GoldenJsonFilter.txt --inputLumiJSON {pileupFile} --calcMode true --minBiasXsec 69200 --maxPileupBin 100 --numPileupBins 100 pileupStuff/pileupProfileData_2016Legacy_Run{era}.root"
+    print(cmd)
